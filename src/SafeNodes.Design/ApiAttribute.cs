b87a40fce9c0f2387f0ba9504b0da20f1d2199dc
Hervé Zucchinetti <hz@swissmediapartners.ch>
2024-09-05 09:23:07 +0200

    Add ApiAttribute.cs documentation

--- src/SafeNodes.Design/ApiAttribute.cs
+++ src/SafeNodes.Design/ApiAttribute.cs
@@ -2,10 +2,17 @@
 
 namespace SafeNodes.Design;
 
+/// <summary>
+/// Define the underlying element as part of the nodes system API, which can be referenced.
+/// </summary>
+/// <param name="reference">The locally unique reference. Make sure to use a stable value.</param>
 [PublicAPI]
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
 [MeansImplicitUse]
 public sealed class ApiAttribute(string reference) : Attribute
 {
+    /// <summary>
+    /// The API member reference.
+    /// </summary>
     public string Reference { get; } = reference;
 }
